2a97fc0e671e0c88cc15bd376434814f0898bba8
OptimusOO <86035121+OptimusOO@users.noreply.github.com>
2021-06-21 16:48:53 +0800

    Add files via upload

--- pydatadc_2018-tidy-master/data/exercise.py
+++ pydatadc_2018-tidy-master/data/exercise.py
@@ -2,7 +2,7 @@ import pandas as pd
 from pandas.io.clipboards import read_clipboard
 from pandas.io.parsers import read_csv
 
-df= pd.read_csv('./gapminder.tsv',sep='\t')
+df= pd.read_csv('../data/gapminder.tsv',sep='\t')
 
 df.columns
 
@@ -47,6 +47,7 @@ billboard_melt.head()
 print(billboard.shape)
 
 
+
 print(billboard_melt.shape)
 
 ebola=pd.read_csv('../data/country_timeseries.csv')
@@ -69,3 +70,42 @@ weather_melt=weather.melt(id_vars=['id','year','month',	'element'],var_name='day
 weather_tidy=weather_melt.pivot_table(index=['id','year','month','day'],columns='element',values='value')
 type(weather_tidy)
 weather_tidy.reset_index()
+
+billboard_melt.head()
+
+billboard_song=billboard_melt[['year','artist','track','time']]
+billboard_song.loc[billboard_song['track']=='Loser']
+billboard_song=billboard_song.drop_duplicates()
+
+print(billboard_melt.shape)
+print(billboard_song.shape)
+
+billboard_song['id']=range(len(billboard_song))
+billboard_song.head()
+
+billboard_song.to_csv('../data/billboard_song.csv',index=False)
+
+billboard_rating=billboard_melt.merge(
+    billboard_song,how='left',on=['year','artist','track','time']
+)
+
+billboard_rating=billboard_rating[['id','date.entered','Week','count']]
+billboard_rating.drop_duplicates()
+billboard_rating.to_csv('../data/billboard_rating.csv',index=False)
+
+billboard_melt.info()
+billboard_song.info()
+billboard_rating.info()
+
+print(ebola.head())
+
+print(ebola['Date'].max())
+print(ebola['Date'].min())
+print(ebola.info())
+
+
+ebola_melt['Date']=pd.to_datetime(ebola_melt['Date'], format="%m/%d/%Y")
+
+print(ebola_melt.head())
+
+print(ebola_melt.groupby([ebola_melt['Date'].dt.year,'status'])['Day'].sum())
